5b7c8eada31f4e54e1d60968b737716ebfa9fbdd
Nikita Adresatov <152248195+Adresatov1998@users.noreply.github.com>
2024-10-27 11:55:14 +0300

    Create PHP.php

---
+++ PHP.php
@@ -0,0 +1,21 @@
+echo $city = 'moscow';
+$url = "http://meteor.serversys.ru/v1?city=$city";
+$response = file_get_contents($url);
+if ($response !== false) {
+    $data = json_decode($response, true);
+    if (isset($data['current'])) {
+        $temperature = $data['current']['temp_c'];
+        $humidity = $data['current']['humidity'];
+        $wind_speed = $data['current']['wind_kph'];
+        echo "Температура: $temperature°C
+Влажность: $humidity%
+Скорость ветра: $wind_speed км/ч
+";
+    } else {
+        echo "Ошибка: данные о текущей погоде недоступны.
+";
+    }
+} else {
+    echo "Ошибка при получении данных о погоде.
+";
+}
